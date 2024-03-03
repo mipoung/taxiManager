efb67b2465c73d9721eaf3423fd8f2554a0b22e8
hoon <mipoung@gmail.com>
2024-03-03 19:51:32 +0900

    기본적인 레이아웃 확정

--- src/main/java/com/taxiManager/controller/MainController.java
+++ src/main/java/com/taxiManager/controller/MainController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.taxiManager.service.MainService;
+import com.taxiManager.vo.DriveInfoVO;
 import com.taxiManager.vo.UserVO;
 
 @Controller
@@ -19,11 +22,52 @@ public class MainController {
 	@GetMapping("/")
 	public String mainPage(Model model) {
 		
-		List<UserVO> list = mainService.mainList();
-		model.addAttribute("list", list);
 		System.out.println("메인페이지 실행");
-		return "index.html";
+		return "views/main/index.html";
 	}
 	
+	@GetMapping("/login")
+	public String mainLogin(Model model) {
+		
+		System.out.println("로그인 페이지 실행");
+		return "views/common/loginPage.html";
+	}
+	
+	
+	@GetMapping("/driveInfoInsert")
+	public String driveInfoInsert(Model model) {
+		
+		System.out.println("운행정보 입력 페이지 실행");
+		
+		return "views/common/driveInfoInsert.html";
+	}
+	
+	@PostMapping("/infoInsert")
+	public String infoInsert(@ModelAttribute DriveInfoVO driveInfoVO) {
+		System.out.println("뷰에서 받아온 값 확인 : \n" + driveInfoVO.getNowKm() + "\n" +driveInfoVO.getTodayIncome() + "\n"+ driveInfoVO.getTodayFuel() + "\n"+ driveInfoVO.getTodayMemo());
+		// 입력 받고 운행정보 보기 페이지로 이동
+		return "views/common/driveInfo.html";
+	}
+	
+	
+	@GetMapping("/driveInfo")
+	public String driveInfo(Model model) {
+		
+		System.out.println("운행정보 보기 페이지 실행");
+		
+		List<UserVO> list = mainService.mainList();
+		model.addAttribute("list", list);
+		System.out.println(list.toString());
+		
+		return "views/common/driveInfo.html";
+	}
+	
+	
+	@GetMapping("/driveStatistics")
+	public String driveStatistics(Model model) {
+		
+		System.out.println("운행통계 보기 페이지 실행");
+		return "views/common/driveStatistics.html";
+	}
 	
 }
---
+++ src/main/java/com/taxiManager/vo/DriveInfoVO.java
@@ -0,0 +1,57 @@
+package com.taxiManager.vo;
+
+public class DriveInfoVO {
+	
+	String totalKm; // 총 주행거리
+	String nowKm; // 현재 주행거리
+	String todayIncome; // 오늘 수입
+	String todayFuel; // 오늘 연료비
+	String netProfit; // 순이익
+	String todayMemo; // 오늘 메모
+	
+	
+	
+	public String getTotalKm() {
+		return totalKm;
+	}
+	public void setTotalKm(String totalKm) {
+		this.totalKm = totalKm;
+	}
+	public String getNowKm() {
+		return nowKm;
+	}
+	public void setNowKm(String nowKm) {
+		this.nowKm = nowKm;
+	}
+	public String getTodayIncome() {
+		return todayIncome;
+	}
+	public void setTodayIncome(String todayIncome) {
+		this.todayIncome = todayIncome;
+	}
+	public String getTodayFuel() {
+		return todayFuel;
+	}
+	public void setTodayFuel(String todayFuel) {
+		this.todayFuel = todayFuel;
+	}
+	public String getNetProfit() {
+		return netProfit;
+	}
+	public void setNetProfit(String netProfit) {
+		this.netProfit = netProfit;
+	}
+	public String getTodayMemo() {
+		return todayMemo;
+	}
+	public void setTodayMemo(String todayMemo) {
+		this.todayMemo = todayMemo;
+	}
+	
+	
+	
+	
+
+	
+
+}
