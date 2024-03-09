d1352cc5f151fcdfb35ed6fd7fb6659cb76b408a
YoungHoon <mipoung@gmail.com>
2024-03-09 16:14:01 +0900

    데이터베이스 변수명 변경

--- src/main/java/com/taxiManager/vo/UserVO.java
+++ src/main/java/com/taxiManager/vo/UserVO.java
@@ -3,7 +3,7 @@ package com.taxiManager.vo;
 public class UserVO {
 	private int id;
     private String username;
-    private String taxiNumber;
+    private String CarNumber;
     private String email;
     private String operationShift;
     private String phoneNum;
@@ -17,7 +17,7 @@ public class UserVO {
         return "UserVO{" +
                 "id=" + id +
                 ", username='" + username + '\'' +
-                ", taxiNumber='" + taxiNumber + '\'' +
+                ", taxiNumber='" + CarNumber + '\'' +
                 ", email='" + email + '\'' +
                 ", operationShift='" + operationShift + '\'' +
                 ", phoneNum='" + phoneNum + '\'' +
@@ -27,54 +27,6 @@ public class UserVO {
     }
     
     
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getTaxiNumber() {
-		return taxiNumber;
-	}
-	public void setTaxiNumber(String taxiNumber) {
-		this.taxiNumber = taxiNumber;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getOperationShift() {
-		return operationShift;
-	}
-	public void setOperationShift(String operationShift) {
-		this.operationShift = operationShift;
-	}
-	public String getPhoneNum() {
-		return phoneNum;
-	}
-	public void setPhoneNum(String phoneNum) {
-		this.phoneNum = phoneNum;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getCreatedAt() {
-		return createdAt;
-	}
-	public void setCreatedAt(String createdAt) {
-		this.createdAt = createdAt;
-	}
 	
     
 
